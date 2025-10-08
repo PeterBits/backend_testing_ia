ad96af8a8cfcc24561c3420f0997e33d4d0597ae
PeterBits <pedroparra06091994@gmail.com>
2025-10-08 23:08:47 +0200

    Add global language constants
    Define language IDs for internationalization support:
    - English: 2
    - Spanish: 4

---
+++ src/utils/constants.js
@@ -0,0 +1,16 @@
+/**
+ * Global constants for the GAINZ application
+ */
+
+/**
+ * Language IDs
+ * These correspond to the language IDs in the database
+ */
+const LANGUAGES = {
+  ENGLISH: 2,
+  SPANISH: 4,
+};
+
+module.exports = {
+  LANGUAGES,
+};
